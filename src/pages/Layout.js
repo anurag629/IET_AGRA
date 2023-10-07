0af12767544bdf98fdece68e17f698fe16e4665a
anurag629 <akvermaav629@gmail.com>
2023-10-07 10:04:07 +0530

    updated everything

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,16 @@
 // import { CssBaseline } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import CustomNavbar from "../components/header/CustomNavbar";
 import Navbar from "../components/Navbar";
+import Footer from "../components/footer/Footer";
 
 const Layout = () => {
   return <>
     {/* <CssBaseline /> */}
     <Navbar />
+    {/* <CustomNavbar /> */}
     <Outlet />
+    <Footer />
   </>;
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
     "./node_modules/flowbite/**/*.js",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+ 
   ],
   theme: {
     extend: {},
@@ -11,5 +15,6 @@ module.exports = {
     require('flowbite/plugin'),
     require('tailwindcss'),
     require('autoprefixer'),
+    require("tw-elements/dist/plugin.cjs"),
   ],
-};
+});
