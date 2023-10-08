2de5320783d068a54ce369037d5c3081b47819aa
anurag629 <akvermaav629@gmail.com>
2023-10-09 03:40:27 +0530

    improved the tand p dahsboard

--- src/components/TandPCell/CellComponent/Placements.js
+++ src/components/TandPCell/CellComponent/Placements.js
@@ -2,35 +2,30 @@ import React from 'react';
 
 const Placements = () => {
     return (
-        <div class="flex flex-wrap items-center">
-            <div class="w-full md:w-5/12 px-4 mx-auto">
-                <div class="  px-4 mr-auto">
-
-                    <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                        Placement Stats
-                    </h3>
-                    <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
-                        Don't let your uses guess by attaching tooltips and popoves to
-                    </p>
-                    <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
-                        The kit comes with three pre-built pages to help you get started
-                        faster. You can change the text and images and you're good to
-                        go. Just make sure you enable them first via JavaScript.
-                    </p>
-
-                </div>
-                <table >
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div className="flex flex-wrap items-center justify-center">
+            <div className='p-8'>
+                <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                    Placement Stats
+                </h3>
+                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+                    Don't let your users guess by attaching tooltips and popovers to
+                </p>
+                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
+                    The kit comes with three pre-built pages to help you get started
+                    faster. You can change the text and images, and you're good to
+                    go. Just make sure you enable them first via JavaScript.
+                </p>
+                <table className="w-full">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3 block whitespace-nowrap">
+                            <th scope="col" className="px-6 py-3 block whitespace-nowrap">
                                 Student Name
                             </th>
-                            <th scope="col" class="px-6 py-3 ">
+                            <th scope="col" className="px-6 py-3">
                                 Company
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Placement/Internship
-
                             </th>
                             <th>
                                 Package
@@ -38,41 +33,38 @@ const Placements = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" >
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row">
                                 Anurag Verma
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 DataWars
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Data Science Intern
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $99999
                             </td>
                         </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" >
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row">
                                 Aman
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Google
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 SDE
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $1999
                             </td>
                         </tr>
-
                     </tbody>
                 </table>
             </div>
         </div>
-
-
     );
 };
 
