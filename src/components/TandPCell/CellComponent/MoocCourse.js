0f0af885803fd6cb83cc16d2a14d7aeb9226d2a8
Bhavy Airi <airiragahv@gmail.com>
2023-10-12 10:23:53 +0530

    connect api(s)

--- src/components/TandPCell/CellComponent/MoocCourse.js
+++ src/components/TandPCell/CellComponent/MoocCourse.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const MoocCourse = () => {
+    const [courses, setCourses] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false)
+  
+    useEffect(() => {
+      fetch("https://placement-site.onrender.com/api/tnp/courses/list_all/")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setCourses(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+          }
+        )
+    }, [])
+  
     return (
         <div className="flex flex-wrap items-center justify-center">
             <div className='p-8'>
                 <h3 className="text-3xl mb-2 font-semibold leading-normal">
                     Mooc Courses completed by students of IET Agra
                 </h3>
-                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
-                    Don't let your users guess by attaching tooltips and popovers to
-                </p>
-                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
-                    The kit comes with three pre-built pages to help you get started
-                    faster. You can change the text and images, and you're good to
-                    go. Just make sure you enable them first via JavaScript.
-                </p>
                 <table className="w-full">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -22,45 +31,33 @@ const MoocCourse = () => {
                                 Student Name
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                Company
+                                Course Name
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                Placement/Internship
+                                Course Fee
                             </th>
                             <th>
-                                Package
+                                Course Duration
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        {courses.map((course, index) => (
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
                             <th scope="row">
-                                Anurag Verma
+                                {course.name}
                             </th>
                             <td className="px-6 py-4">
-                                DataWars
+                                {course.course_name}
                             </td>
                             <td className="px-6 py-4">
-                                Data Science Intern
+                                {course.course_fee}
                             </td>
                             <td className="px-6 py-4">
-                                $99999
+                                {course.course_duration}
                             </td>
                         </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row">
-                                Aman
-                            </th>
-                            <td className="px-6 py-4">
-                                Google
-                            </td>
-                            <td className="px-6 py-4">
-                                SDE
-                            </td>
-                            <td className="px-6 py-4">
-                                $1999
-                            </td>
-                        </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
