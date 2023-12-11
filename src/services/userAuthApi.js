f6a5356b820e7389d24fa29bd77063daa24ed93b
Bhavy Airi <airiragahv@gmail.com>
2023-12-11 14:14:52 +0530

    Connecting api

--- src/services/userAuthApi.js
+++ src/services/userAuthApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const userAuthApi = createApi({
   reducerPath: 'userAuthApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://placement-site.onrender.com/api/account/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://ietagra-backend.onrender.com/api/account/' }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (user) => {
