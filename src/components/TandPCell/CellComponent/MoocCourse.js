1bbb8e3aca7727cd3db1c8c515438f746df169e6
anurag629 <akvermaav629@gmail.com>
2023-12-13 15:18:20 +0530

    added filters

--- src/components/TandPCell/CellComponent/MoocCourse.js
+++ src/components/TandPCell/CellComponent/MoocCourse.js
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from 'react';
 
 const MoocCourse = () => {
     const [courses, setCourses] = useState([]);
+    const [batchFilter, setBatchFilter] = useState('');
+    const [platformFilter, setPlatformFilter] = useState('');
+    const [branchFilter, setBranchFilter] = useState('');
+    const [filteredCourses, setFilteredCourses] = useState([]);
 
     useEffect(() => {
         fetch("https://project-iet-tnp-bk.vercel.app/api/course/course-list-approved/")
             .then(res => res.json())
             .then(
                 (result) => {
-                    console.log(result);
                     setCourses(result);
+                    setFilteredCourses(result);
                 },
                 (error) => {
                     console.log(error);
@@ -19,12 +23,83 @@ const MoocCourse = () => {
     }, [])
 
 
+    const handleFilter = () => {
+        const filteredData = courses.filter(course => {
+            return (
+                (batchFilter === '' || course.fields.student_batch === batchFilter) &&
+                (platformFilter === '' || course.fields.course_platform === platformFilter) &&
+                (branchFilter === '' || course.fields.student_branch === branchFilter)
+            );
+        });
+        setFilteredCourses(filteredData);
+    };
+
+    const batchOptions = [...new Set(courses.map(course => course.fields.student_batch))];
+    const platformOptions = [...new Set(courses.map(course => course.fields.course_platform))];
+    const branchOptions = [...new Set(courses.map(course => course.fields.student_branch))];
+
+
     return (
         <div className="flex flex-wrap items-center justify-center">
             <div className='p-8'>
                 <h3 className="text-3xl mb-2 font-semibold leading-normal">
                     MOOC Courses completed by our students.
                 </h3>
+
+                <div className="flex flex-col mb-4">
+                    <div className="flex items-center mb-4">
+                        <label className="px-6 py-3 block whitespace-nowrap">Batch:</label>
+                        <select
+                            value={batchFilter}
+                            onChange={(e) => setBatchFilter(e.target.value)}
+                            className="border p-1"
+                        >
+                            <option value="">All</option>
+                            {batchOptions.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="flex items-center mb-4">
+                        <label className="px-6 py-3 block whitespace-nowrap">Platform:</label>
+                        <select
+                            value={platformFilter}
+                            onChange={(e) => setPlatformFilter(e.target.value)}
+                            className="border p-1"
+                        >
+                            <option value="">All</option>
+                            {platformOptions.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="flex items-center mb-4">
+                        <label className="px-6 py-3 block whitespace-nowrap">Branch:</label>
+                        <select
+                            value={branchFilter}
+                            onChange={(e) => setBranchFilter(e.target.value)}
+                            className="border p-1"
+                        >
+                            <option value="">All</option>
+                            {branchOptions.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+
+                        <button onClick={handleFilter} className="bg-blue-500 text-white px-4 py-2 rounded ml-4">
+                            Apply Filters
+                        </button>
+                    </div>
+                </div>
+
                 <table className="w-full">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -52,7 +127,7 @@ const MoocCourse = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {courses.map((course, index) => (
+                        {filteredCourses.map((course, index) => (
                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
                                 <th scope="row" className="px-6 py-4">
                                     {course.fields.student_batch}
--- src/components/TandPCell/CellComponent/Placements.js
+++ src/components/TandPCell/CellComponent/Placements.js
@@ -3,7 +3,12 @@ import { useState, useEffect } from 'react';
 
 const Placements = () => {
     const [placements, setPlacements] = useState([]);
-    const [imageError, setImageError] = useState(false);
+    const [batchFilter, setBatchFilter] = useState('');
+    const [companyFilter, setCompanyFilter] = useState('');
+    const [branchFilter, setBranchFilter] = useState('');
+    const [filteredPlacements, setFilteredPlacements] = useState([]);
+    const [sortBy, setSortBy] = useState(''); // 'package' or ''
+    const [sortOrder, setSortOrder] = useState(''); // 'asc' or 'desc'
 
 
     useEffect(() => {
@@ -11,8 +16,8 @@ const Placements = () => {
             .then(res => res.json())
             .then(
                 (result) => {
-                    console.log(result);
                     setPlacements(result);
+                    setFilteredPlacements(result);
                 },
                 (error) => {
                     console.log(error);
@@ -21,6 +26,48 @@ const Placements = () => {
     }, [])
 
 
+    const handleFilter = () => {
+        const filteredData = placements.filter(placement => {
+            return (
+                (batchFilter === '' || placement.fields.student_batch === batchFilter) &&
+                (companyFilter === '' || placement.fields.company_name === companyFilter) &&
+                (branchFilter === '' || placement.fields.student_branch === branchFilter)
+            );
+        });
+        setFilteredPlacements(filteredData);
+    };
+
+    const batchOptions = [...new Set(placements.map(placement => placement.fields.student_batch))];
+    const companyOptions = [...new Set(placements.map(placement => placement.fields.company_name))];
+    const branchOptions = [...new Set(placements.map(placement => placement.fields.student_branch))];
+
+
+    const handleSort = (column) => {
+        if (column === 'package') {
+            if (sortBy === 'package') {
+                setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+            } else {
+                setSortBy('package');
+                setSortOrder('asc');
+            }
+        }
+    };
+
+    const sortedPlacements = [...filteredPlacements];
+
+
+    if (sortBy === 'package') {
+        sortedPlacements.sort((a, b) => {
+            const aValue = parseFloat(a.fields.student_salary);
+            const bValue = parseFloat(b.fields.student_salary);
+
+            if (sortOrder === 'asc') {
+                return aValue - bValue;
+            } else {
+                return bValue - aValue;
+            }
+        });
+    }
 
     return (
         <div className="flex flex-wrap items-center justify-center">
@@ -28,6 +75,63 @@ const Placements = () => {
                 <h3 className="text-3xl mb-2 font-semibold leading-normal">
                     Placement Statistics
                 </h3>
+
+                <div className="flex flex-col mb-4">
+                    <div className="flex items-center mb-4">
+                        <label className="px-6 py-3 block whitespace-nowrap">Batch:</label>
+                        <select
+                            value={batchFilter}
+                            onChange={(e) => setBatchFilter(e.target.value)}
+                            className="border p-1"
+                        >
+                            <option value="">All</option>
+                            {batchOptions.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="flex items-center mb-4">
+                        <label className="px-6 py-3 block whitespace-nowrap">Company:</label>
+                        <select
+                            value={companyFilter}
+                            onChange={(e) => setCompanyFilter(e.target.value)}
+                            className="border p-1"
+                        >
+                            <option value="">All</option>
+                            {companyOptions.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="flex items-center mb-4">
+                        <label className="px-6 py-3 block whitespace-nowrap">Branch:</label>
+                        <select
+                            value={branchFilter}
+                            onChange={(e) => setBranchFilter(e.target.value)}
+                            className="border p-1"
+                        >
+                            <option value="">All</option>
+                            {branchOptions.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+
+                        <button onClick={handleFilter} className="bg-blue-500 text-white px-4 py-2 rounded ml-4">
+                            Apply Filters
+                        </button>
+                    </div>
+                </div>
+
+
+
                 <table className="w-full">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -49,14 +153,20 @@ const Placements = () => {
                             <th scope="col" className="px-6 py-3">
                                 Position
                             </th>
-                            <th scope="col" className="px-6 py-3">
-                                Package (LPA)
+                            <th scope="col"
+                                className="px-6 py-3"
+                                onClick={() => handleSort('package')}
+                                style={{ cursor: 'pointer' }}
+                            >
+                                <span>Package (LPA)</span>
+                                {sortBy === 'package' && sortOrder === 'asc' && <span>▲</span>}
+                                {sortBy === 'package' && sortOrder === 'desc' && <span>▼</span>}
                             </th>
 
                         </tr>
                     </thead>
                     <tbody>
-                        {placements.map((placement, index) => (
+                        {sortedPlacements.map((placement, index) => (
                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
                                 <th scope="row">
                                     {placement.fields.student_batch}
@@ -70,14 +180,14 @@ const Placements = () => {
                                             src={(placement.fields.profile_picture && placement.fields.profile_picture[0].url)}
                                             alt="student_image"
                                             className="w-12 h-12 rounded-md mr-4"
-                                            
-                                            
+
+
                                         />}
-                                        {!placement.fields.profile_picture && 
+                                        {!placement.fields.profile_picture &&
                                             <img
-                                            src="./images/smile_emoji.jpg"
-                                            alt="student_image"
-                                            className="w-10 h-10 rounded-md mr-4"
+                                                src="./images/smile_emoji.jpg"
+                                                alt="student_image"
+                                                className="w-10 h-10 rounded-md mr-4"
                                             />
                                         }
                                     </div>
