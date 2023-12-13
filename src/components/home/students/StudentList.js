c7e444440a877458e072e1c380dd6e25b93b4a00
anurag629 <akvermaav629@gmail.com>
2023-12-13 22:30:35 +0530

    added toppers

--- src/components/home/students/StudentList.js
+++ src/components/home/students/StudentList.js
@@ -4,29 +4,49 @@ import StudentCard from './StudentCard';
 const StudentList = () => {
     const students = [
         {
-            name: 'Lana Byrd',
-            role: '9.8 CGPA',
-            imageUrl: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png',
+            name: 'Prathviraj Singh',
+            cgpa: '7.5 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/D4D03AQHl2Bz4Qz8DaQ/profile-displayphoto-shrink_800_800/0/1671265473603?e=1707955200&v=beta&t=nJosa1BdQHUHnNuxt_5DGjm6yFvRMGoTOJaEosqpI10',
 
         },
         {
-            name: 'Lana Byrd',
-            role: '9.8 CGPA',
-            imageUrl: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png',
+            name: 'Aman Chauhary',
+            cgpa: '7.8 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/D4D03AQHc0LTDPiG01g/profile-displayphoto-shrink_800_800/0/1689089097278?e=1707955200&v=beta&t=NalPgrANOZUyGAK_s_pyjM-GIjbE-WPy6gQC-E5QL2I',
 
         },
         {
-            name: 'Lana Byrd',
-            role: '9.8 CGPA',
-            imageUrl: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png',
+            name: 'Anurag Verma',
+            cgpa: '7.5 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/D4D03AQFtHKg93wbEtA/profile-displayphoto-shrink_800_800/0/1701904508997?e=1707955200&v=beta&t=j-LW0mx6r9yEVzzMl1ILs78LT3iNLJyKvKZWBktzvg4',
 
         },
         {
-            name: 'Lana Byrd',
-            role: '9.8 CGPA',
-            imageUrl: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png',
+            name: 'Abhishek Gupta',
+            cgpa: '8.8 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/D4D03AQGJK2UJyGAb1g/profile-displayphoto-shrink_800_800/0/1698993271429?e=1707955200&v=beta&t=N9ZMlv210pt5vp79FDhVudXF4jBhYHwtNdHKAy3eFVo',
 
         },
+        {
+            name: 'Bhavya Airi',
+            cgpa: '7.1 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/D4D03AQHUidcg3h_HzQ/profile-displayphoto-shrink_800_800/0/1696473506408?e=1707955200&v=beta&t=2WAWmjSxoBEn5sFt-MqdadlkiA4yJ7MY1Qio53Z_11U',
+        },
+        {
+            name: 'Gurudayal Gautam',
+            cgpa: '8.2 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/D4D03AQFXBvjZ5NF0_w/profile-displayphoto-shrink_800_800/0/1680796502644?e=1707955200&v=beta&t=G7aoP4hP4Vem-GIkWUSbUr0wlWmbRc9-R3rfqBovkcE',
+        },
+        {
+            name: 'Naveen Kumar',
+            cgpa: '6.5 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/C4E03AQGo2oRIExsjgw/profile-displayphoto-shrink_800_800/0/1663817247858?e=1707955200&v=beta&t=aq8UEI_Aa89Tk1vJVAaqSLnzsMNdsX97xbCJmOmqtPc',
+        },
+        {
+            name: 'Himanshu Jaiswal',
+            cgpa: '7.5 CGPA',
+            imageUrl: 'https://media.licdn.com/dms/image/C4D03AQFU3njPQrYw2A/profile-displayphoto-shrink_800_800/0/1628921726721?e=1707955200&v=beta&t=SWDnNr_FbMA_SI7duCNb5UJXXeND12SaIajmtdr4Ziw',
+        }
        
         // Add other students here
     ];
